efd37dc9bb1572299671599d1e44925834444fd4
jiovani fabro <you@example.com>
2023-10-22 23:06:47 +0800

    deployed

--- app/src/main/java/com/example/pasta_shop/MainActivity.kt
+++ app/src/main/java/com/example/pasta_shop/MainActivity.kt
@@ -18,28 +18,94 @@ class MainActivity : AppCompatActivity() {
         val baconcheesebtn = findViewById<Button>(R.id.baconcheesebtn)
         val carbonarabtn = findViewById<Button>(R.id.carbonarabtn)
 
+         val shrimp=199
+        val  spinach=399
+        val  skilleti=299
+        val  marinara=299
+        val  baconcheese=299
+        val  carbonara=299
+
+
+
+
+
+
         shrimpbtn.setOnClickListener {
 
 
             val intent = Intent(this, MainActivity3::class.java)
-            startActivity(intent)
 
+            val imageResourceId=R.drawable.ag
+
+            intent.putExtra("imageResourceId", imageResourceId)
+            intent.putExtra("title", "Shrimp Aglio Olio")
+            intent.putExtra("description", "Olive Oil Coated Pasta Lightly Sautéed With Fresh Garlic Succulent Shrimps And Red Chili Flakes. Served With Garlic Bread.")
+            intent.putExtra("price", 199)
+
+            startActivity(intent)
 
         }
         spinachbtn.setOnClickListener {
-            Toast.makeText(this, "spinachbtn", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, MainActivity3::class.java)
+
+
+            val imageResourceId=R.drawable.pinach
+            intent.putExtra("imageResourceId", imageResourceId)
+            intent.putExtra("title", "Spinach Roll-Ups")
+            intent.putExtra("description", "  Creamy Lasagna rolls layered with spinach and white sauce")
+
+
+            intent.putExtra("price", 299)
+            startActivity(intent)
         }
         skilletibtn.setOnClickListener {
-            Toast.makeText(this, "skilletibtn", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, MainActivity3::class.java)
+            intent.putExtra("price", 299)
+            intent.putExtra("title", "Skilletti")
+            intent.putExtra("description", "  Rich tomato sauce pasta, made more flavorful with chorizo, salami, herbs and spices. Served with garlic bread.")
+
+
+            val imageResourceId=R.drawable.skiletti
+            intent.putExtra("imageResourceId", imageResourceId)
+            startActivity(intent)
         }
         baconcheesebtn.setOnClickListener {
-            Toast.makeText(this, " baconcheesebtn", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, MainActivity3::class.java)
+            intent.putExtra("price", 299)
+            val imageResourceId=R.drawable.baconncheesexvsxray
+            intent.putExtra("title", "Bacon And Cheese Fusilli")
+            intent.putExtra("description", " Italian sausage baked in fusilli pasta with marinara sauce topped with cheddar cheese and parsley. Served with garlic bread")
+
+
+            intent.putExtra("imageResourceId", imageResourceId)
+            startActivity(intent)
         }
         carbonarabtn.setOnClickListener {
-            Toast.makeText(this, "  carbonarabtn", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, MainActivity3::class.java)
+            intent.putExtra("price", 399)
+            intent.putExtra("description", " Creamy white sauce pasta with bacon, ham, and button mushrooms with premium parmesan cheese. Served with garlic bread.")
+
+
+            intent.putExtra("title", "Carbonara Supreme")
+            val imageResourceId=R.drawable.carbonarasupreme
+            intent.putExtra("imageResourceId", imageResourceId)
+            startActivity(intent)
         }
         marinarabtn.setOnClickListener {
-            Toast.makeText(this,   "marinarabt=", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, MainActivity3::class.java)
+            intent.putExtra("price", 299)
+            intent.putExtra("description", "  Premium pasta in chunky marinara sauce with fish cubes, squid rings and shrimps, topped with parmesan cheese. Served with garlic bread.")
+
+
+            intent.putExtra("title", "Seafood Marinara")
+            val imageResourceId=R.drawable.seafoodmarinara
+            intent.putExtra("imageResourceId", imageResourceId)
+            startActivity(intent)
         }
 
 
--- app/src/main/java/com/example/pasta_shop/MainActivity3.kt
+++ app/src/main/java/com/example/pasta_shop/MainActivity3.kt
@@ -2,10 +2,91 @@ package com.example.pasta_shop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+        val dfiamg = findViewById<ImageView>(R.id.dfimage)
+        val productprice = findViewById<TextView>(R.id.productprice)
+        val productdescription = findViewById<TextView>(R.id.description)
+        val producttitle = findViewById<TextView>(R.id.producttitle)
+        val imageResourceId = intent.getIntExtra("imageResourceId", R.id.imageView6)
+        val getprice = "₱" + intent.getIntExtra("price", 0).toString()
+        producttitle.text = intent.getStringExtra("title")
+        productprice.text = getprice
+        val settotal = findViewById<TextView>(R.id.total)
+
+
+
+        productdescription.text = intent.getStringExtra("description")
+        dfiamg.setImageResource(imageResourceId)
+        val box1 = findViewById<CheckBox>(R.id.box1)
+        val box2 = findViewById<CheckBox>(R.id.box2)
+        val box3 = findViewById<CheckBox>(R.id.box3)
+        val box4 = findViewById<CheckBox>(R.id.box4)
+        val box5 = findViewById<CheckBox>(R.id.box5)
+        val box6 = findViewById<CheckBox>(R.id.box6)
+        val box7 = findViewById<CheckBox>(R.id.box7)
+        val box8 = findViewById<CheckBox>(R.id.box8)
+        val totalprice = mutableListOf(intent.getIntExtra("price", 0), 0, 0, 0, 0, 0, 0, 0)
+
+        fun calculateaddons(id: CheckBox, index: Int, Value: Int) {
+
+            if (id.isChecked) {
+                totalprice[index] = Value
+                settotal.text = totalprice.sum().toString()
+            } else {
+                totalprice[index] = 0
+                settotal.text = totalprice.sum().toString()
+
+            }
+
+        }
+        settotal.text = getprice
+        box2.setOnCheckedChangeListener { button, b ->
+            calculateaddons(box2, 1, 28)
+        }
+
+        box3.setOnCheckedChangeListener { button, b ->
+            calculateaddons(box3, 2, 65)
+        }
+
+        box4.setOnCheckedChangeListener { button, b ->
+
+            calculateaddons(box4, 3, 48)
+        }
+
+        box5.setOnCheckedChangeListener { button, b ->
+            //group
+            if (box5.isChecked) {
+                totalprice[0] = 699
+                settotal.text = totalprice.sum().toString()
+            } else {
+                totalprice[0] = intent.getIntExtra("price", 0)
+                settotal.text = totalprice.sum().toString()
+            }
+
+
+        }
+
+        box6.setOnCheckedChangeListener { button, b ->
+            calculateaddons(box6, 5, 68)
+        }
+
+        box7.setOnCheckedChangeListener { button, b ->
+
+            calculateaddons(box7, 6, 38)
+        }
+
+        box8.setOnCheckedChangeListener { button, b ->
+
+            calculateaddons(box8, 7, 58)
+        }
+
     }
 }
